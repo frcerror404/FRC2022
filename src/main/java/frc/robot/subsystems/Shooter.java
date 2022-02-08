b75b8db414905ebd72b0b8ba62e7f1302c992895
Waverly Robotics <waverlyrobotics@gmail.com>
2022-02-07 20:44:33 -0500

    Inverted shooter driving me crazy

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -7,6 +7,7 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
@@ -21,25 +22,35 @@ public class Shooter extends SubsystemBase {
    */
   private final WPI_TalonFX shooterMotorA = new WPI_TalonFX(Constants.kShooterA);
   private final WPI_TalonFX shooterMotorB = new WPI_TalonFX(Constants.kShooterB);
-  
+  private boolean _inverted_test = false;
 
   public Shooter() {
-    shooterMotorA.setInverted(TalonFXInvertType.Clockwise);
     shooterMotorB.follow(shooterMotorA);
     shooterMotorB.setInverted(TalonFXInvertType.OpposeMaster);
+
+    System.out.println("Shooter 1 Inverted? " + (shooterMotorA.getInverted() ? "True" : "False"));
+    shooterMotorA.setInverted(false);
+    System.out.println("Shooter 1 Inverted? " + (shooterMotorA.getInverted() ? "True" : "False"));
   }
   
   /**
    * @param ShooterSpeed the speed 0-1
    */
   public void setShooterSpeed(double shooterSpeed) {
+
+    // if(shooterSpeed > 0) {
+    //   _inverted_test = !_inverted_test;
+    //   shooterMotorA.setInverted(_inverted_test);
+    // }
+    System.out.println("Shooter 1 Inverted? " + (shooterMotorA.getInverted() ? "True" : "False"));
+
     shooterMotorA.set(shooterSpeed);
   }
   
   @Override
   public void periodic() {
-    System.out.println(String.format("Shooter 1 Speed: %f rpm", shooterMotorA.getSensorCollection().getIntegratedSensorVelocity()));
-    System.out.println(String.format("Shooter 1 Inverted?", shooterMotorA.getInverted()));
+    //System.out.println(String.format("Shooter 1 Speed: %f rpm", shooterMotorA.getSensorCollection().getIntegratedSensorVelocity()));
+    //System.out.println(String.format("Shooter 1 Inverted?", shooterMotorA.getInverted()));
     // This method will be called once per scheduler run
   }
 }
